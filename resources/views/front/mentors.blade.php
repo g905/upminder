cbcf6d40397b6a4194ddfd919db3582878c8157d
g905 <breakestra@yandex.ru>
2022-12-24 09:44:18 +0400

    добавил ссылки на карточку ментора - ава, фио и подробнее

--- resources/views/front/mentors.blade.php
+++ resources/views/front/mentors.blade.php
@@ -2,19 +2,19 @@
     <div class="row">
         <div class="col-lg-2 col-md-3  d-none d-md-block">
             <div style="text-align: center; position: relative">
-                <a href="cart.html">
+                <a href="{{ route('front.mentor', ['id' => $mentor->id]) }}">
                     <span class="mentorday">Ментор дня</span><img src="{{ Storage::disk('public')->url('avatar/' . $mentor->avatar) }}" class="img-fluid">
                 </a>
             </div>
         </div>
         <div class="col-lg-7 col-md-5">
             <div class="d-block d-md-none" style="float:left; width: 30%; border:0px solid red; margin-right: 8px; margin-top: 20px;">
-                <a href="cart.html">
+                <a href="{{ route('front.mentor', ['id' => $mentor->id]) }}">
                     <span class="mentorday">Ментор дня</span><img src="{{ Storage::disk('public')->url('avatar/' . $mentor->avatar) }}" class="img-fluid">
                 </a>
             </div>
             <h3>
-                <a href="cart.html">{{ $mentor->first_name }} {{ $mentor->last_name }}</a>
+                <a href="{{ route('front.mentor', ['id' => $mentor->id]) }}">{{ $mentor->first_name }} {{ $mentor->last_name }}</a>
                 @if($mentor->verified)
                 <span class='verified'>
                     <img src='{{ asset("assets/images/cart_icon/verify.svg") }}' alt="verify-icon">
@@ -51,7 +51,7 @@
                 <div class="sale">Цена по запросу</div>
                 @endif
                 <div class="btn_block">
-                    <a href="cart.html" class="enter">Подробнее</a>
+                    <a href="{{ route('front.mentor', ['id' => $mentor->id]) }}" class="enter">Подробнее</a>
                     <a href="#" class="request" data-bs-toggle="modal" data-bs-target="#personalmentormodal">Оставить заявку</a>
                 </div>
             </div>
